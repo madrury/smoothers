42fa3305543e67e3c2b326ea707b99221fe98904
madrury <matthew.drury.83@gmail.com>
2016-06-24 17:42:51 -0700

    Add smoother-ui

---
+++ js/smoother-ui.js
@@ -0,0 +1,31 @@
+smoother_ui = function(smoothers) {
+
+    return {
+
+        /* Message queue for the controller. */
+        msg_queue: [];
+
+        /* Bind the smoother UI to a select input dom element, render the possible
+         * selections, and then listen for changes. 
+         */
+        bind: function(e) {
+            // TODO: assert that e is an select input.
+            element = e;
+            for(let smooth_type in smoothers) {
+                if(smoothers.hasOwnProperty(smooth_type)) {
+                    e.innerHTML += '<option value="' + smooth_type + '">' + 
+                                   smoothers[smooth_type]["label"] + 
+                                   '</option>';
+                }
+            }
+            // Bind a listener for change events.
+            d3.select("select#smooth-type").on("change", function() 
+            {
+                let smoother = e.options[e.selectedIndex].value];
+                this.msg_queue.push({"smoother-change": smoother});
+            })
+        },
+   
+    };
+
+}
