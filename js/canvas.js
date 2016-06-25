7d23759faec08ddcad9d623f4c21a1c74ab9e218
madrury <matthew.drury.83@gmail.com>
2016-06-24 17:20:50 -0700

    Add message queue to canvas.js.
      Also some cleanups, restructure with module pattern.

--- js/canvas.js
+++ js/canvas.js
@@ -1,137 +1,140 @@
-// A class representing a physical canvas which contains datapoints, and
-// on which a smoothing of those datapoints can be rendered.
-let Canvas = function(elem, dimensions, margins) {
+/* An object representing a physical canvas which contains datapoints, and
+   on which a smoothing of those datapoints can be rendered.
 
-    // Physical properties of the canvas
-    this.height = dimensions.height;
-    this.width = dimensions.width;
-    this.margins = margins;
-    this.xscale = d3.scale.linear()
+   The canvas responds to clicks from teh user by rendering a new datapoint
+   at the location of the click, and storing the x, y coordinates of the new
+   point as internal state.  When enough points have been stored, clicks also
+   cause the canvas to send a "smooth" message to a controller, which will
+   respond with the smoother and parameters to use for drawing a smoother.
+ */
+let canvas = function(elem, dimensions, margins) {
+
+    /* Physical properties of the canvas */
+    height = dimensions.height;
+    width = dimensions.width;
+    margins = margins;
+    xscale = d3.scale.linear()
         .domain([0, 1])
         .range([margins.left, dimensions.width - margins.right]);
-    this.xaxis = d3.svg.axis()
-        .scale(this.xscale)
+    xaxis = d3.svg.axis()
+        .scale(xscale)
         .orient("bottom")
         .ticks(10);
-    this.yscale = d3.scale.linear()
+    yscale = d3.scale.linear()
         .domain([0, 1])
         .range([dimensions.height - margins.bottom, margins.top]);
-    this.yaxis = d3.svg.axis()
-        .scale(this.yscale)
+    yaxis = d3.svg.axis()
+        .scale(yscale)
         .orient("left")
         .ticks(10);
 
-    // Physical manifestation of the canvas
-    this.svg = d3.select(elem).append("svg")
+    svg = d3.select(elem).append("svg")
         .attr("height", dimensions.height)
         .attr("width", dimensions.width);
-    this.svg.append("g")
+    svg.append("g")
         .attr("class", "axis")
         .attr("transform", "translate(0, " + (dimensions.height - margins.top) + ")")
-        .call(this.xaxis);
-    this.svg.append("g")
+        .call(xaxis);
+    svg.append("g")
         .attr("class", "axis")
         .attr("transform", "translate(" + margins.left + ", 0)")
-        .call(this.yaxis);
+        .call(yaxis);
 
-    // Data associated with the canvas. (x, y) is the collection of user drawn
-    // points; (xhat, yhat) is a smoothing of those points.  
-    this.x = [];
-    this.xhat = d3.range(0, 1, .01)
-    this.y = [];
-    this.yhat = [];
+    /* Data associated with the canvas. (x, y) is the collection of user drawn
+     * points; (xhat, yhat) is a smoothing of those points.  
+     */
+    x = [];
+    xhat = d3.range(0, 1, .01)
+    y = [];
+    yhat = [];
 
-    // Smoothing function currently associated with the canvas
-    this.smoother = undefined;
+    /* Is there a currently rendered smoother? */
+    hasbeensmoothed = false;
 
-    // Is there a currently rendered smoother?
-    this.hasbeensmoothed = false;
 
-    // Bind an event handler to the canvasr: a click even on the physical canvas
-    // draws a physical point, add adds the coordinates of the point to the x and
-    // y attributes.  Note that the method add_point resides on the prototype.
-    let that = this
-    this.svg.on("click", function() {
-        let p = d3.mouse(this);
-        that.add_point(p[0], p[1])
-    });
-}
+    return {
 
-Canvas.prototype = {
+        /* Message queue for the contorller object. */
+        msg_queue: [],
 
-    constructor: Canvas,
+        /* Bind a click handler to the canvas which will respond to a mouse
+         * click by rendering a point and possibly drawing a smoothed curve.
+         */
+        bind_click_listener: function() {
+            svg.on("click", function() {
+                let p = d3.mouse(this);
+                this.add_point(p[0], p[1])
+            });
+        },
 
-    // Draw a point on the canvas and update the internal state.
-    add_point: function(px, py) {
-        this.x.push(this.xscale.invert(px));
-        this.y.push(this.yscale.invert(py));
-        this.svg.append("circle")
-            .attr("cx", px)
-            .attr("cy", py)
-            .attr("r", 3)
-            .attr("id", "data-point");
-        if(this.x.length > 2) {
-            this.smooth();
+        /* Draw a point on the canvas and update the internal state. */
+        add_point: function(px, py) {
+            x.push(xscale.invert(px));
+            y.push(yscale.invert(py));
+            svg.append("circle")
+                .attr("cx", px)
+                .attr("cy", py)
+                .attr("r", 3)
+                .attr("id", "data-point");
+            if(x.length > 2) {
+                this.msg_queue.push({"smooth": null})
+            }
+        },
+
+        /* Remove everything from the canvas and clear all applicable state. */
+        clear: function() {
+            x = [];
+            y = [];
+            yhat = [];
+            svg.selectAll('circle').remove();
+            svg.selectAll('path#smoothed-path').remove();
+            hasbeensmoothed = false;
+        },
+
+        /* Smooth the x, y data drawn on the canvas with a certain smoothing
+         * algorithm in accord with certain parameters. 
+         */
+        smooth: function(smoother, parameters) {
+            console.log(smoother)
+            // Only makes sense to smooth more than two points, if less, bail early.
+            if(x.length <= 1) {
+                return;
+            }
+            // Smooth the data points
+            yhat = smoother.smoother(parameters)(x, y)(xhat);
+            let data = d3.zip(xhat, yhat);
+            let line = d3.svg.line()
+                             .x(d => xscale(d[0]))
+                             .y(d => yscale(d[1]))
+                             .interpolate("linear")
+
+            if(!hasbeensmoothed) {
+                // If no smoothed graph has been rendered yet, render it.
+                svg.append("path")
+                    .attr("d", line(data))
+                    .attr("stroke", "lightskyblue")
+                    .attr("stroke-width", 2)
+                    .attr("fill", "none")
+                    .attr("id", "smoothed-path");
+                hasbeensmoothed = true;
+            } else {
+                // If there is already a smoothed graph, render it with a transition
+                svg.selectAll("path#smoothed-path")
+                    .transition()
+                    .duration(1000)
+                    .attr("d", line(data))
+                    .attr("stroke", "lightskyblue")
+                    .attr("stroke-width", 2)
+                    .attr("fill", "none");
+            }
+        },
+
+        /* Clear the smoother drawn on the canvas and clear all applicable state. */
+        clear_smooth: function() {
+            yhat = []
+            svg.selectAll('circle#data-smoother').remove();
+            hasbeensmoothed = false;
         }
-    },
 
-    // Change the smoothing algorithm.
-    set_smoother: function(smooother) {
-        this.smoother = smoother;
-        this.smooth()
-    },
-
-    // Remove everything from the canvas.
-    clear: function() {
-        this.x = [];
-        this.y = [];
-        this.yhat = [];
-        this.svg.selectAll('circle').remove();
-        this.svg.selectAll('path#smoothed-path').remove();
-        this.hasbeensmoothed = false;
-    },
-
-    // Smooth the data drawn on the canvas with a certain smoothing algorithm.
-    // The smoother argument should process two arrays representing the x and
-    // y data to be smoothed, and return a function that, when applied to an
-    // array of new x values, returns the associated smoothed y values.
-    smooth: function() {
-        // Only makes sense to smooth more than two points, if less, bail early.
-        if(this.x.length <= 1) {return;}
-        // Smooth the data points
-        this.yhat = this.smoother(this.x, this.y)(this.xhat);
-        let data = d3.zip(this.xhat, this.yhat);
-        that = this;  // Preserve for method chain.
-        let line = d3.svg.line()
-                     .x(function(d) {return that.xscale(d[0]);})
-                     .y(function(d) {return that.yscale(d[1]);})
-                     .interpolate("linear")
-
-        if(!this.hasbeensmoothed) {
-            // If no smoothed graph has been rendered yet, render it.
-            this.svg.append("path")
-                .attr("d", line(data))
-                .attr("stroke", "lightskyblue")
-                .attr("stroke-width", 2)
-                .attr("fill", "none")
-                .attr("id", "smoothed-path");
-            this.hasbeensmoothed = true;
-        } else {
-            // If there is already a smoothed graph, render it with a transition
-            this.svg.selectAll("path#smoothed-path")
-                .transition()
-                .duration(1000)
-                .attr("d", line(data))
-                .attr("stroke", "lightskyblue")
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
-        }
-    },
-
-        // Clear the smoother drawn on the canvas.
-    clear_smooth: function() {
-        this.yhat = []
-        this.svg.selectAll('circle#data-smoother').remove();
-        this.hasbeensmoothed = false;
-    }
+    };
 };
