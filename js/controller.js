1a0eea1018631cb58ff097c5896f9a36e11f0ecc
madrury <matthew.drury.83@gmail.com>
2016-07-05 19:43:20 -0700

    Initial commit of controller.

---
+++ js/controller.js
@@ -0,0 +1,40 @@
+let controller = function(canvas, smoother_ui, parameter_ui) {
+
+    return {
+
+        /* Start the event loop, listening for messages from the various ui
+         * elements.
+         */
+        run: function() {
+            let that = this;
+            window.setInterval(function() {
+                if(canvas.msg_queue.length > 0) {
+                    msg = canvas.msg_queue.pop();
+                    that.field_canvas_msg(msg);
+                }
+                if(smoother_ui.msg_queue.length > 0) {
+                    msg = smoother_ui.msg_queue.pop();
+                    that.field_smoother_ui_msg(msg);
+                }
+                if(parameter_ui.msg_queue.length > 0) {
+                    msg = parameter_ui.msg_queue.pop();
+                    that.field_parameter_ui_msg(msg);
+                }
+            });
+        },
+
+        field_canvas_msg: function(msg) {
+            console.log("message from canvas " + msg);
+        },
+
+        field_smoother_ui_msg: function(msg) {
+            console.log("message from smoother ui  " + msg);
+        },
+
+        field_parameter_ui_msg: function(msg) {
+            console.log("message from parameter ui  " + msg);
+        },
+
+    }
+
+}
