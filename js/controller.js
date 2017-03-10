117c82ae25cfd0f3f7a04c1d3c09cbaf51d674d7
Matthew Drury <matthew.drury@galvanize.com>
2017-03-09 17:07:46 -0800

    Improve documentation on interface elements.

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,17 @@
+/* A contorller object to coordinate communicaton between the canvas and the
+   various user interface elements.
+
+   This object has two main responsibilities:
+     - Monitor objects in the application for messages in their msg_queue
+       lists.
+     - Respond to these messages in by taking the appropriate actions.
+*/
 let controller = function(canvas, smoother_ui, parameter_ui) {
 
     return {
 
         /* Start the event loop, listening for messages from the various ui
-         * elements.
+           elements.
          */
         run: function() {
             let that = this;
@@ -23,6 +31,11 @@ let controller = function(canvas, smoother_ui, parameter_ui) {
             });
         },
 
+        /* Respond to a message from the canvas object.
+
+           At the moment, there is only one possible message from the canvas, a
+           request to draw a smoother.
+        */
         field_canvas_msg: function(msg) {
             let smoother_name = smoother_ui.get_selected_smoother(); 
             let smoother = smoothers[smoother_name];
@@ -30,6 +43,13 @@ let controller = function(canvas, smoother_ui, parameter_ui) {
             canvas.smooth(smoother, parameters);
         },
 
+        /* Respond to a message from the smoother ui object.
+
+           There are two possible messages from the smoother ui:
+             - "smoother-change": Sent when the user selects a new smoothing
+               algorithm.
+             - "smoother-clear": Sent when the user clicks the "clear" button.
+        */
         field_smoother_ui_msg: function(msg) {
             if ("smoother-change" in msg) {
                 let smoother_name = smoother_ui.get_selected_smoother();
@@ -47,6 +67,11 @@ let controller = function(canvas, smoother_ui, parameter_ui) {
             }
         },
 
+        /* Respond to messages from the parameter ui object.
+
+           At the moment, there is only one possible message from the parameter
+           ui, sent when the user makes a change to some hyperparameter.
+        */
         field_parameter_ui_msg: function(msg) {
             let smoother_name = smoother_ui.get_selected_smoother(); 
             let smoother = smoothers[smoother_name];
@@ -56,7 +81,7 @@ let controller = function(canvas, smoother_ui, parameter_ui) {
             if (smoother.hasOwnProperty("knot_function")) {
                 canvas.draw_knots(smoother.knot_function, parameters["n"])
             }
-        },
+        }
 
     }
 
